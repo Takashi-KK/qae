74ce65b59d22ae9a5062154054df670cbbdc83e7
Takashi K.K <112754674+Takashi-KK@users.noreply.github.com>
2024-06-12 19:18:26 +0900

    #3 ipv4first, dns.lookup

--- frontend/get_ip.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-MyIP=`ip -4 addr show wlp0s20f3 | grep inet | awk '{print $2}' | cut -d/ -f1`
-echo $MyIP
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+
+dns.lookup("github.com", { all: true }, (err, addresses) => {
+  console.log("[next.config.mjs]: dns.lookup()");
+  console.log({ addresses, err });
+});
+
 const nextConfig = {};
 
 export default nextConfig;
